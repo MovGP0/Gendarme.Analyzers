d84f4ca4439a049ee5a55531ecaaed7f03f3d5f8
Johann Dirry <johann.dirry@microsea.at>
2025-09-25 14:35:09 +0200

    adding documentation for the IsIntegralType method

--- Gendarme.Analyzers/TypeSymbolExtensions.cs
+++ Gendarme.Analyzers/TypeSymbolExtensions.cs
@@ -2,6 +2,13 @@ namespace Gendarme.Analyzers;
 
 public static class TypeSymbolExtensions
 {
+    /// <summary>
+    /// Checks if a given type symbol represents an integral type.
+    /// </summary>
+    /// <returns>
+    /// <c>true</c> if the type symbol is any of the integral types
+    /// (<c>sbyte</c>, <c>byte</c>, <c>short</c>, <c>ushort</c>, <c>int</c>, <c>uint</c>, <c>long</c>, or <c>ulong</c>).
+    /// </returns>
     public static bool IsIntegralType(this ITypeSymbol typeSymbol)
     {
         return typeSymbol.SpecialType switch
